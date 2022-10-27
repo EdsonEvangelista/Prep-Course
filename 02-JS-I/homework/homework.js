f05978ff2253613ec710bd3ac02ef7fd3ac2a736
Edson Evangelista <practicante1@administradorasjt.com>
2022-10-27 11:21:38 -0500

    Homework number 1 Done..!

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x == y) {
+  if (x === y) {
     return true;
   }else {
     return false;
@@ -71,7 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.Length == str2.Length) {
+  if (str1.length === str2.length) {
     return true;
   }else {
     return false;
@@ -110,7 +110,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 == 0) {
+  if(num % 2 === 0) {
     return true;
   }else {
     return false;
@@ -121,7 +121,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 != 0) {
+  if (num % 2 !== 0) {
     return true;
   }else {
     return false;
@@ -162,7 +162,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return Math.random;
+  return Math.random();
 }
 
 function esPositivo(numero) {
@@ -198,7 +198,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var nuevaCadena = "Hola " + nombre;
+  var nuevaCadena = "Hola " + nombre + "!";
   return nuevaCadena;
 }
 
@@ -222,7 +222,7 @@ function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
   var areaTriangulo = (base * altura) / 2;
-  return areaDelTriangulo;
+  return areaTriangulo;
 }
 
 
@@ -241,15 +241,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.Length != 1) {
-    if (letra.toLowerCase() == a || letra.toLowerCase() == e || letra.toLowerCase() == i || letra.toLowerCase() == o || letra.toLowerCase() == u ) {
-      return "Es vocal";
-    } else {
-      return "Dato incorrecto";
-    }
-  }else {
-    return "Dato incorrecto";
+  if(letra.length > 1){
+    return "Dato incorrecto"
   }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 }
 
 
